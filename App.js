e87dde31dc2fd1754a8ba24722e7bfbdb8ab49ab
GaelGrezz <gutierrezgael838@gmail.com>
2025-02-18 21:53:03 -0500

    Homework finished

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const postList = async () => {
+      const resp = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const post = await resp.json();
+      setPosts(post);
+    };
+
+    postList();
+  }, []);
+
+  const showPost = ({ item }) => (
+    <View style={styles.itemContainer}>
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.body}>{item.body}</Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" />
+      <Text>MyList</Text>
+      <FlatList
+        data={posts}
+        renderItem={showPost}
+        keyExtractor={item => item.id.toString()}
+      />
     </View>
   );
 }
@@ -16,5 +41,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  itemContainer: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  body: {
+    fontSize: 16,
+    color: '#555',
   },
 });
--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-A02 - Listas React Native
+A02 - Listas React Native 
 # FlatList
 
+## ¿Qué es FlatList?
+Es un componente que optimiza el renderizado de listas de gran volumen de datos en la interfaz.
+
+## Características de FlatList
+Sus características son numerosas, pero se enfocan principalmente en el apartado visual para un mejor renderizado.   
+A continuación, se presentan algunas de sus características más importantes:
+- Es un componente multiplataforma.
+- Soporte para modo horizontal.
+- Carga de elementos al desplazarse.
+- Soporte para varias columnas.
+- Soporte para actualización en tiempo real de los elementos en lista.
+
+## ¿Cómo funciona?
+~~~
+const App = () => (
+  <FlatList
+    data={DATA}
+    renderItem={renderItem}
+    keyExtractor={item => item.id}
+  />
+);
+~~~
+- data: Esta propiedad recibe un array de datos que permite renderizar los elementos de la lista.
+
+- renderItem: Define cómo se renderiza cada uno de los elementos en la lista. 
+
+- keyExtractor: Muestra cómo debe tomarse la clave de cada elemento de la lista. 
